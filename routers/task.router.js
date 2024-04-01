465c748dd485aa54d05d76180774e63eefcada8e
urooj khan <khanurooj5299@gmail.com>
2024-04-01 16:13:27 +0530

    Created route for getting all tasks

--- routers/task.router.js
+++ routers/task.router.js
@@ -3,4 +3,10 @@ const TaskModel = require("../models/task.model");
 
 const router = express.Router();
 
+router.get("/tasks", (req, res, next) => {
+  TaskModel.find({})
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
 module.exports = router;
