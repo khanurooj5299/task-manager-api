716c082e51b8ed3aef7f9b0541da654fcf041c4f
urooj khan <khanurooj5299@gmail.com>
2024-04-01 23:44:29 +0530

    Added error handling to add-task route

--- routers/task.router.js
+++ routers/task.router.js
@@ -9,4 +9,15 @@ router.get("/tasks", (req, res, next) => {
     .catch(next);
 });
 
+router.post("/add-task", (req, res, next) => {
+  console.log(req.body)
+  if (req.body && req.body.title && req.body.deadline) {
+    TaskModel.create(req.body)
+      .then((insertedTask) => res.json(insertedTask))
+      .catch(next);
+  } else {
+    throw { status: 400, message: "Incorrect request body" };
+  }
+});
+
 module.exports = router;
