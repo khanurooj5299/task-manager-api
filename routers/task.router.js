7f9ab375e34cfb04dcb07f9797266097ee62684a
urooj khan <khanurooj5299@gmail.com>
2024-04-02 05:29:08 +0530

    Added endpoint for updating a task

--- routers/task.router.js
+++ routers/task.router.js
@@ -10,10 +10,27 @@ router.get("/tasks", (req, res, next) => {
 });
 
 router.post("/add-task", (req, res, next) => {
-  console.log(req.body)
   if (req.body && req.body.title && req.body.deadline) {
     TaskModel.create(req.body)
       .then((insertedTask) => res.json(insertedTask))
+      .catch(next);
+  } else {
+    throw { status: 400, message: "Incorrect request body" };
+  }
+});
+
+router.put("/update-task/:updateId", (req, res, next) => {
+  const task = req.body;
+  const id = req.params.updateId;
+  if (task && task.title && task.deadline) {
+    TaskModel.updateOne({ _id: id }, task)
+      .then((result) => {
+        if (result.modifiedCount == 1) {
+          res.json({ status: 200, message: "Successfully updated" });
+        } else {
+          res.json({ status: 404, message: "No such document" });
+        }
+      })
       .catch(next);
   } else {
     throw { status: 400, message: "Incorrect request body" };
