e841ea45480022877f41164c4f6d33f3223f6ffd
urooj khan <khanurooj5299@gmail.com>
2024-04-01 16:08:09 +0530

    Created connection.js file to connect to mongodb database

---
+++ utilities/connection.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb://localhost:27017/virtualization_dashboard_db";
+
+//connectionPromise is a promise which resolves with undefined when the connection is successful and rejects with the mongodb error, when it is not
+const connectionPromise = mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connection to DB created succesfully.");
+  })
+  .catch((err) => {
+    console.log("Connection to DB failed!");
+    console.error(err);
+    throw err;
+  });
+
+function disconnect() {
+  mongoose.connection.close();
+}
+
+module.exports.connect = connectionPromise;
+module.exports.disconnect = disconnect;
